b8233591fbade9893ba25b989361552d7ce3908c
= <=>
2024-05-09 11:30:51 -0400

    conexion a la base de datos

---
+++ config.py
@@ -0,0 +1,4 @@
+#conexion  a la base de datos
+
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root@localhost/db_usuarios'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
--- run.py
+++ run.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import config
 
 app= Flask(__name__)
+app.config.from_object(config)
+db = SQLAlchemy(app)
+
 
 @app.route('/')
 def index():
-    return "Hello World!"
+    return "inicio"
 
 
 
